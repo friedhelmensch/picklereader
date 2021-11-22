ff078c0838efe79540ce568535275c55fb781310
Hannes <hannes@rang.de>
2021-11-22 16:53:21 +0100

    using supabase database

---
+++ api/address/[address].js
@@ -0,0 +1,10 @@
+const getHistory = require("../../lib/history");
+
+module.exports = async (req, res) => {
+  if (req.method === "GET") {
+    const { address } = req.query;
+    var history = await getHistory(address);
+    return res.status(200).send(history);
+  }
+  return res.status(400).send("not supported");
+};
