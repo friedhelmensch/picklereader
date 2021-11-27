5eb6b1d61a2d97ff4acc190fba8fc71c6c978312
Hannes <hannes@rang.de>
2021-11-27 22:30:20 +0100

    adding repository function to get balance history entry based on date

--- test/balanceDifferenceToYesterday.test.js
+++ test/balanceDifferenceToYesterday.test.js
@@ -1,11 +1,21 @@
 const readDaiBalance = require("../lib/readDaiBalance");
 jest.mock("../lib/readDaiBalance");
-readDaiBalance.mockResolvedValue(280);
+
+const historyRepository = require("../lib/repository/historyRepository");
+jest.mock("../lib/repository/historyRepository");
+
+const currentBalance = 280;
+const historicalBalance = 180;
+
+readDaiBalance.mockResolvedValue(currentBalance);
+historyRepository.getBalanceHistoryEntry.mockResolvedValue({
+  amount: historicalBalance,
+});
 
 const getBalanceDifference = require("../lib/balanceDifferenceToYesterday");
 
 test("get difference to yesterday returns 140", async () => {
   expect(
     await getBalanceDifference("0xb5d85cbf7cb3ee0d56b3bb207d5fc4b82f43f511")
-  ).toBe(140);
+  ).toBe(100);
 });
--- test/repository/historyRepository.test.js
+++ test/repository/historyRepository.test.js
@@ -2,29 +2,49 @@ const {
   writeBalanceHistoryEntry,
   getBalanceHistory,
   deleteBalanceHistory,
+  getBalanceHistoryEntry,
 } = require("../../lib/repository/historyRepository");
 
 test("write entries and delete entries", async () => {
-  const newEntry = await writeBalanceHistoryEntry(
-    "0xb5d85cbf7cb3ee0d56b3bb207d5fc4b82f43f511",
-    900
-  );
+  const testAddress = "0xb5d85cbf7cb3ee0d56b3bb207d5fc4b82f43f511";
+
+  const newEntry = await writeBalanceHistoryEntry(testAddress, 900);
   expect(newEntry.amount).toBe(900);
 
-  await writeBalanceHistoryEntry(
-    "0xb5d85cbf7cb3ee0d56b3bb207d5fc4b82f43f511",
-    6547
-  );
-  const balanceHistory = await getBalanceHistory(
-    "0xb5d85cbf7cb3ee0d56b3bb207d5fc4b82f43f511"
-  );
+  await writeBalanceHistoryEntry(testAddress, 6547);
+  const balanceHistory = await getBalanceHistory(testAddress);
   expect(balanceHistory.length).toBe(2);
 
-  await deleteBalanceHistory("0xb5d85cbf7cb3ee0d56b3bb207d5fc4b82f43f511");
+  await deleteBalanceHistory(testAddress);
 
-  const balanceHistoryAfterDelete = await getBalanceHistory(
-    "0xb5d85cbf7cb3ee0d56b3bb207d5fc4b82f43f511"
-  );
+  const balanceHistoryAfterDelete = await getBalanceHistory(testAddress);
 
   expect(balanceHistoryAfterDelete.length).toBe(0);
+});
+
+test("write 3 entries 2 hours apart and get one in the middle", async () => {
+  const testAddress = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
+
+  const twentyOne = new Date(new Date().getTime() - 21 * 60 * 60 * 1000);
+  const twentythree = new Date(new Date().getTime() - 23 * 60 * 60 * 1000);
+  const twentyFive = new Date(new Date().getTime() - 25 * 60 * 60 * 1000);
+
+  const twentyFour = new Date(new Date().getTime() - 24 * 60 * 60 * 1000);
+
+  await writeBalanceHistoryEntry(testAddress, 900, twentyOne);
+
+  const secondEntry = await writeBalanceHistoryEntry(
+    testAddress,
+    6547,
+    twentythree
+  );
+
+  await writeBalanceHistoryEntry(testAddress, 999, twentyFive);
+
+  const entry = await getBalanceHistoryEntry(testAddress, twentyFour);
+
+  expect(entry).toMatchObject(secondEntry);
+
+  //clean up
+  await deleteBalanceHistory(testAddress);
 });
